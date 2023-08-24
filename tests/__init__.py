6ab9751f0f40e5c43bf958c08184d054a1d22916
sam <sam.woodcock@protonmail.com>
2023-08-24 16:36:41 +0100

    test: update docstrings in test files

---
+++ tests/__init__.py
@@ -0,0 +1,1 @@
+"""Test osm-rawdata."""
--- tests/test_json.py
+++ tests/test_json.py
@@ -2,7 +2,7 @@
 
 # Copyright (c) 2023 Humanitarian OpenStreetMap Team
 #
-# This file is part of osm_fieldwork.
+# This file is part of osm_rawdata.
 #
 #     This is free software: you can redistribute it and/or modify
 #     it under the terms of the GNU General Public License as published by
@@ -15,19 +15,19 @@
 #     GNU General Public License for more details.
 #
 #     You should have received a copy of the GNU General Public License
-#     along with osm_fieldwork.  If not, see <https:#www.gnu.org/licenses/>.
+#     along with osm_rawdata.  If not, see <https:#www.gnu.org/licenses/>.
 #
+"""Test JSON functionality."""
 
 import os
-import sys
-from osm_rawdata.config import QueryConfig
-
 
 # Find the other files for this project
 import osm_rawdata as rw
+from osm_rawdata.config import QueryConfig
+
 rootdir = rw.__path__[0]
-if os.path.basename(rootdir) == 'osm_rawdata':
-    rootdir = f"./tests/"
+if os.path.basename(rootdir) == "osm_rawdata":
+    rootdir = "./tests/"
 # print(f"\t{rootdir}")
 
 
@@ -38,32 +38,35 @@ def test_levels():
     qc.dump()
     assert qc.filename == "Example export with all features" and qc.outputtype == "geojson"
 
+
 def test_filters():
     # this query contains only the geometry and the output file name and type
     qc = QueryConfig()
     qc.parseJson(f"{rootdir}/filters.json")
     qc.dump()
     hits = 0
-    if 'name' in qc.config['select'][0]:
+    if "name" in qc.config["select"][0]:
         hits += 1
-    if 'addr' in qc.config['select'][1]:
+    if "addr" in qc.config["select"][1]:
         hits += 1
-    if 'building' in qc.config['select'][2]:
+    if "building" in qc.config["select"][2]:
         hits += 1
-    if 'cafe' in qc.config['select'][3]['amenity']:
+    if "cafe" in qc.config["select"][3]["amenity"]:
         hits += 1
-    if 'restaurant' in qc.config['select'][3]['amenity']:
+    if "restaurant" in qc.config["select"][3]["amenity"]:
         hits += 1
-    if 'pub' in qc.config['select'][3]['amenity']:
+    if "pub" in qc.config["select"][3]["amenity"]:
         hits += 1
 
     assert hits == 6
-    
+
+
 def test_formats():
     # this query contains only the geometry and the output file name and type
     qc = QueryConfig()
     qc.parseJson(f"{rootdir}/formats.json")
     assert qc.outputtype == "shp" and qc.filename == "Pokhara_all_features"
+
 
 def test_everything():
     # this query contains only the geometry, we want everything within this polygon
@@ -71,6 +74,7 @@ def test_everything():
     qc.parseJson(f"{rootdir}/everything.json")
     data = "POLYGON ((83.96919250488281 28.194446860487773, 83.99751663208006 28.194446860487773, 83.99751663208006 28.214869548073377, 83.96919250488281 28.214869548073377, 83.96919250488281 28.194446860487773))"
     assert data == str(qc.geometry)
+
 
 if __name__ == "__main__":
     print("--- test_everything() ---")
@@ -81,5 +85,3 @@ if __name__ == "__main__":
     test_levels()
     print("--- test_filters ---")
     test_filters()
-
-    

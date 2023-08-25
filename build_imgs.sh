c7eba40e82152af1913df412ddda8bcd3ee51a5a
sam <sam.woodcock@protonmail.com>
2023-08-25 16:07:21 +0100

    build: separate build and push for ci image

--- build_imgs.sh
+++ build_imgs.sh
@@ -4,12 +4,16 @@ set -eo pipefail
 
 PKG_VERSION=0.1.0
 
-docker build . --push \
+docker build . \
     -t "ghcr.io/hotosm/osm-rawdata:${PKG_VERSION}" \
     --target prod \
     --build-arg PKG_VERSION="${PKG_VERSION}"
 
-docker build . --push \
+docker push "ghcr.io/hotosm/osm-rawdata:${PKG_VERSION}"
+
+docker build . \
     -t ghcr.io/hotosm/osm-rawdata:ci \
     --target ci \
     --build-arg PKG_VERSION="${PKG_VERSION}"
+
+docker push ghcr.io/hotosm/osm-rawdata:ci
